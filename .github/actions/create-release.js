48aabcfeff3507cc54fac38ba1e91338a5205572
onkarg <onkarg@gmail.com>
2022-05-20 15:37:30 -0700

    push to develop

--- .github/actions/create-release.js
+++ .github/actions/create-release.js
@@ -1,13 +1,42 @@
 const { Octokit } = require("@octokit/action");
 
-const octokit = new Octokit();
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const main = async () => {
+  try {
+    const octokit = new Octokit();
+    const tag = process.env.RELEASE_TAG;
+    const message = `Fleet Weekly Release - ${tag}`;
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    const branch = process.env.GITHUB_REF.replace("/refs/heads/", "");
+    const object = process.env.GITHUB_SHA;
 
-// See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  owner,
-  repo,
-  title: "My test issue",
-});
+    // See https://docs.github.com/en/rest/git/tags#create-a-tag-object
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/git/tags",
+      {
+        owner,
+        repo,
+        tag,
+        message,
+        object,
+        type: "commit",
+      }
+    );
 
-console.log("Issue created: %s", data.html_url);
+    // See https://docs.github.com/en/rest/releases/releases#create-a-release
+    await octokit.request('POST /repos/{owner}/{repo}/releases', {
+      owner,
+      repo,
+      tag_name: tag,
+      target_commitish: branch,
+      name: message,
+      body: '# Description of the release',
+      draft: false,
+      prerelease: false,
+      generate_release_notes: true
+    })
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+main();
